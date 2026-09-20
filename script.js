fb9258874bce3621ba8d5f67ea68d43e30410561
Kundan Kumar <kkroy969@gmail.com>
2026-09-20 20:16:42 +0530

    Add initial implementation of dropdown menu with styling and functionality

---
+++ script.js
@@ -0,0 +1,6 @@
+let acountBtn = document.getElementById("accountBtn");
+let dropdown = document.getElementById("dropdown");
+
+acountBtn.addEventListener("click", function(){
+    dropdown.classList.toggle("show");
+})
